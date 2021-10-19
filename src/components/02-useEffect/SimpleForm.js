2d3e0a162c466a032d900516286d7d4e74d40317
jantonio2 <jesusrochavedia@gmail.com>
2021-10-18 21:26:38 -0400

    useEffect montando y desmontando componentes

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './effects.css'
+import { Message } from './Message';
 
 export const SimpleForm = () => {
 
@@ -11,15 +12,15 @@ export const SimpleForm = () => {
   const {name, email} = formState;
 
   useEffect(() => {
-    console.log('hey!')
+    // console.log('hey!')
   }, []);
 
   useEffect(() => {
-    console.log('formState cambio')
+    // console.log('formState cambio')
   }, [formState]);
 
   useEffect(() => {
-    console.log('email cambio')
+    // console.log('email cambio')
   }, [email]);
 
   const handleInputChange = ({target}) => {
@@ -58,6 +59,8 @@ export const SimpleForm = () => {
           onChange = {handleInputChange}
         />
       </div>
+
+      { (name === '123') && <Message />}
     </>
   )
 }
