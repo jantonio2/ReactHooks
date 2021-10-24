cd8b71809c6970cb4f2098e7907c59804c66586e
jantonio2 <jesusrochavedia@gmail.com>
2021-10-23 22:14:09 -0400

    Basico useRef hook

---
+++ src/components/04-useRef/FocusScreen.js
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react'
+import './effects.css'
+
+export const FocusScreen = () => {
+
+  const inputRef = useRef();
+  console.log(inputRef);
+
+  const handleClick = () => {
+    // document.querySelector('input').focus();
+    // document.querySelector('input').select();
+    inputRef.current.select();
+  };
+
+  return (
+    <>
+      <h1>Focus Screen</h1> 
+      <hr />
+
+      <input 
+        ref = {inputRef}
+        className = "form-control"
+        placeholder = "Su nombre"
+      />     
+
+      <button 
+        className = "btn btn-outline-primary mt-5"
+        onClick = {handleClick}
+      >
+        Focus
+      </button>
+    </>
+  )
+}
