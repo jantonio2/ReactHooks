bb784f311ba75a7c2695ca19676c06554db8688d
jantonio2 <jesusrochavedia@gmail.com>
2021-10-26 17:25:29 -0400

    useCallback utilizado junto con React.memo y useEffect

---
+++ src/components/06-memos/CallbackHook.js
@@ -0,0 +1,29 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { ShowIncrement } from './ShowIncrement';
+import './memorize.css'
+
+export const CallbackHook = () => {
+
+  const [counter, setCounter] = useState(10);
+
+  // const increment = () => {
+  //   setCounter(counter + 1);
+  // }
+
+  const increment = useCallback((num) => {
+    setCounter(c => c + num);
+  }, [setCounter]);
+
+  useEffect(() => {
+    //??
+  }, [increment])
+
+  return (
+    <>
+      <h1>useCallback Hook: {counter}</h1>
+      <hr />
+
+      <ShowIncrement increment = {increment} />  
+    </>
+  )
+}
