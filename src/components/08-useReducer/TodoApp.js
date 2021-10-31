589868f0a0531b5605c63181a530cd4fea79dc42
jantonio2 <jesusrochavedia@gmail.com>
2021-10-30 21:50:08 -0400

    Formulario de TODO creado

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -16,14 +16,51 @@ export const TodoApp = () => {
 
   return (
     <>
-      <h1>TodoApp</h1>
+      <h1>TodoApp ({todos.length})</h1>
       <hr />
 
-      <ul>
-        <li>Hola</li>
-        <li>Mundo</li>
-        <li>Hola de nuevo</li>
-      </ul>      
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {
+              todos.map((todo, i) => {
+                return (<li
+                  key="todo.id"
+                  className="list-group-item"
+                >
+                  <p className="text-center">{i + 1}. {todo.desc}</p>
+                  <button
+                    className="btn btn-danger"
+                  >
+                      Borrar
+                  </button>
+                </li>)
+              })
+            }
+          </ul>  
+        </div>
+
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr/>
+
+          <form className="d-grid gap-2">
+            <input 
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder="Aprender ..."
+              autoComplete="off"
+            />
+
+            <button
+              className="btn btn-outline-primary mt-1"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>    
     </>
   )
 }
